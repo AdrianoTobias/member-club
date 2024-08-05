406f448f244dda1a43caaa2fd5b93511f3886c32
Adriano <adrianoact@msn.com>
2024-08-04 21:21:55 -0300

    add load card section

--- src/modules/Member/load.js
+++ src/modules/Member/load.js
@@ -1,8 +1,12 @@
 const userSection = document.getElementById("user");
+const cardSection = document.getElementById("card");
 const main = document.querySelector("main");
 
 export function load({ id, name, clientSince, appointmentHistory, loyaltyCard}) {
+    const { totalCuts } = loyaltyCard;
+
     loadUserSection({ id, name, clientSince });
+    loadCardSection({ id, totalCuts });
 
     main.style.display = "grid";
 }
@@ -17,4 +21,40 @@ function loadUserSection({ id, name, clientSince }) {
     img.src = userAvatar;
     h2.textContent = name;
     p.textContent = `Cliente desde ${clientSince}`;
+}
+
+function loadCardSection({ id, totalCuts }) {
+    const totalSlots = 10;
+
+    const h2 = cardSection.querySelector("#tag-id h2");
+    const slots = cardSection.querySelector("#slots");
+
+    slots.textContent = "";
+
+    h2.textContent = `ID: ${id}`;
+
+    for (let i = 1; i <= totalSlots; i++) {
+        const div = document.createElement("div");
+        const img = document.createElement("img");
+
+        const hasUserCut = i <= totalCuts;
+        const isLastSlot = i === totalSlots;
+
+        let image = null;        
+
+        if (hasUserCut)
+            image = "PinCheck.png";
+        else
+            image = isLastSlot ? "GiftSolid.svg" : null;
+        
+        div.classList.add("item");        
+        img.alt = `Slot ${i}`;
+
+        if (image) {
+            img.src = `src/assets/${image}`;
+            div.append(img);    
+        }            
+        
+        slots.append(div);        
+    }
 }
