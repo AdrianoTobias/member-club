1d20b9049c79efed50058a5a087954b22eafab01
Adriano <adrianoact@msn.com>
2024-08-04 21:37:58 -0300

    add load history section

--- src/modules/Member/load.js
+++ src/modules/Member/load.js
@@ -1,6 +1,7 @@
 const userSection = document.getElementById("user");
 const cardSection = document.getElementById("card");
 const progressSection = document.getElementById("progress");
+const historySection = document.getElementById("history");
 const main = document.querySelector("main");
 
 export function load({ id, name, clientSince, appointmentHistory, loyaltyCard}) {
@@ -9,6 +10,7 @@ export function load({ id, name, clientSince, appointmentHistory, loyaltyCard}) 
     loadUserSection({ id, name, clientSince });
     loadCardSection({ id, totalCuts });    
     loadProgressSection(loyaltyCard);
+    loadHistorySection(appointmentHistory);
 
     main.style.display = "grid";
 }
@@ -70,4 +72,45 @@ function loadProgressSection ({ totalCuts, cutsNeeded, cutsRemaining }) {
     p.textContent = `${totalCuts} de ${cutsNeeded}`;
 
     progressIndicator.style.width = `${totalCuts * cutsNeeded}%`;
+}
+
+function loadHistorySection(appointmentHistory) {
+    const totalCuts = appointmentHistory.length;  
+
+    const p = historySection.querySelector("#history header p");
+    const ul = historySection.querySelector("#history ul");
+
+    p.textContent = `${totalCuts} cortes`
+    ul.textContent = "";
+
+    for (const appointment of appointmentHistory) {
+        const date = appointment.date;
+        const time = appointment.time;
+
+        const li = document.createElement("li");
+        const divTitle = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+        const divContainer = document.createElement("div");
+        const img = document.createElement("img");
+                
+        h2.textContent = date;
+        p.textContent = time;
+        
+        divTitle.classList.add("title");
+        divTitle.append(h2);
+        divTitle.append(p);        
+
+        img.src = "src/assets/icons/sealCheck.svg";
+        img.alt = "Selo de check";
+
+        divContainer.classList.add("seal-check-container");
+        divContainer.append(img);
+        
+        li.classList.add("cut");
+        li.append(divTitle);
+        li.append(img);
+
+        ul.append(li);
+    }
 }
